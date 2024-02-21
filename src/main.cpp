517fc323c1a1fdcf999ca55031e969d8fad227bc
jessc21 <yc3721@ic.ac.uk>
2024-02-21 12:41:23 +0000

    task1

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,83 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
 
-//Constants
-  const uint32_t interval = 100; //Display update interval
+// Constants
+const uint32_t interval = 100; // Display update interval
 
-//Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+// Pin definitions
+// Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+// Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+// Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+// Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
 
-//Display driver object
+// Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-//Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value) {
-      digitalWrite(REN_PIN,LOW);
-      digitalWrite(RA0_PIN, bitIdx & 0x01);
-      digitalWrite(RA1_PIN, bitIdx & 0x02);
-      digitalWrite(RA2_PIN, bitIdx & 0x04);
-      digitalWrite(OUT_PIN,value);
-      digitalWrite(REN_PIN,HIGH);
-      delayMicroseconds(2);
-      digitalWrite(REN_PIN,LOW);
+std::bitset<4> readCols()
+{
+  digitalWrite(RA0_PIN, LOW);
+  digitalWrite(RA1_PIN, LOW);
+  digitalWrite(RA2_PIN, LOW);
+  digitalWrite(REN_PIN, HIGH);
+  std::bitset<4> result;
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
+  return result;
 }
 
-void setup() {
+// select a given row of the switch matrix by setting the value of each row select address pin.
+void setRow(uint8_t rowIdx)
+{
+  digitalWrite(REN_PIN, LOW);
+  digitalWrite(RA0_PIN, rowIdx & 0x01);
+  digitalWrite(RA1_PIN, rowIdx & 0x02);
+  digitalWrite(RA2_PIN, rowIdx & 0x04);
+  digitalWrite(REN_PIN, HIGH);
+}
+
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+  digitalWrite(REN_PIN, LOW);
+  digitalWrite(RA0_PIN, bitIdx & 0x01);
+  digitalWrite(RA1_PIN, bitIdx & 0x02);
+  digitalWrite(RA2_PIN, bitIdx & 0x04);
+  digitalWrite(OUT_PIN, value);
+  digitalWrite(REN_PIN, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(REN_PIN, LOW);
+}
+
+void setup()
+{
   // put your setup code here, to run once:
 
-  //Set pin directions
+  // Set pin directions
   pinMode(RA0_PIN, OUTPUT);
   pinMode(RA1_PIN, OUTPUT);
   pinMode(RA2_PIN, OUTPUT);
@@ -67,36 +94,55 @@ void setup() {
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
+  // Initialise display
+  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
   delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
+  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
   u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
-  //Initialise UART
+  // Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   static uint32_t next = millis();
   static uint32_t count = 0;
+  std::bitset<32> inputs;
 
-  while (millis() < next);  //Wait for next interval
+  while (millis() < next)
+    ; // Wait for next interval
 
   next += interval;
 
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
+  // Update display
+  u8g2.clearBuffer();                   // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  u8g2.drawStr(2, 10, "Helllo World!"); // write something to the internal memory
+  u8g2.setCursor(2, 20);
+  // u8g2.print(count++);
+  // Place a for loop around your call to readCols().
+  // This will be the key scanning loop and it should loop over the row numbers 0 to 2.
+  // For each row, it should set the row select address then read the columns and
+  // copy the results into the inputs bitset
+  // loop over 1,2,4
+  for (int i = 0; i < 8; i++)
+  {
+    setRow(i);
+    std::bitset<4> temp = readCols();
+    inputs[i * 4] = temp[0];
+    inputs[i * 4 + 1] = temp[1];
+    inputs[i * 4 + 2] = temp[2];
+    inputs[i * 4 + 3] = temp[3];
+  }
+  std::bitset<4> inputs = readCols();
+  u8g2.setCursor(2, 20); // print the result on the OLED display at coordinates (2,20)
+  u8g2.print(inputs.to_ulong(), HEX);
+  u8g2.sendBuffer(); // transfer internal memory to the display
 
-  //Toggle LED
+  // Toggle LED
   digitalToggle(LED_BUILTIN);
-  
 }
