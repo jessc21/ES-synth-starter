09a0ee9b77a76c1ef7dc7f8810b3445402ce73c5
jessc21 <yc3721@ic.ac.uk>
2024-02-27 17:31:27 +0000

    lab1 make sound

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <bitset>
+#include <cmath>
+//#include <STM32HardwareTimer.h>
 
 // Constants
 const uint32_t interval = 100; // Display update interval
@@ -33,15 +35,13 @@ const int DRST_BIT = 4;
 const int HKOW_BIT = 5;
 const int HKOE_BIT = 6;
 
+volatile uint32_t currentStepSize;
+
 // Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
 std::bitset<4> readCols()
 {
-  digitalWrite(RA0_PIN, LOW);
-  digitalWrite(RA1_PIN, LOW);
-  digitalWrite(RA2_PIN, LOW);
-  digitalWrite(REN_PIN, HIGH);
   std::bitset<4> result;
   result[0] = digitalRead(C0_PIN);
   result[1] = digitalRead(C1_PIN);
@@ -73,6 +73,17 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value)
   digitalWrite(REN_PIN, LOW);
 }
 
+// Write a new function that will update the phase accumulator and 
+// set the analogue output voltage at each sample interval
+// The function will be triggered by an interrupt 22,000 times per second. 
+// It will add currentStepSize to the phase accumulator to generate the output waveform.
+void sampleISR() {
+  static uint32_t phaseAcc = 0;
+  phaseAcc += currentStepSize;
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -94,6 +105,13 @@ void setup()
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
 
+  // Initialise timer interrupt
+  TIM_TypeDef *Instance = TIM1;
+  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer->attachInterrupt(sampleISR);
+  sampleTimer->resume();
+
   // Initialise display
   setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
   delayMicroseconds(2);
@@ -105,6 +123,46 @@ void setup()
   Serial.begin(9600);
   Serial.println("Hello World");
 }
+
+// Function to calculate step size based on frequency
+uint32_t calculateStepSize(float frequency)
+{
+    return static_cast<uint32_t>((std::pow(2, 32) * frequency) / 22000);
+}
+
+// Define frequencies for each note (in Hz)
+const float noteFrequencies[] = {
+    261.63, // C
+    277.18, // C#
+    293.66, // D
+    311.13, // D#
+    329.63, // E
+    349.23, // F
+    369.99, // F#
+    392.00, // G
+    415.30, // G#
+    440.00, // A
+    466.16, // A#
+    493.88  // B
+};
+
+// Calculate and store step sizes for each note
+const uint32_t stepSizes[] = {
+    calculateStepSize(noteFrequencies[0]), // C
+    calculateStepSize(noteFrequencies[1]), // C#
+    calculateStepSize(noteFrequencies[2]), // D
+    calculateStepSize(noteFrequencies[3]), // D#
+    calculateStepSize(noteFrequencies[4]), // E
+    calculateStepSize(noteFrequencies[5]), // F
+    calculateStepSize(noteFrequencies[6]), // F#
+    calculateStepSize(noteFrequencies[7]), // G
+    calculateStepSize(noteFrequencies[8]), // G#
+    calculateStepSize(noteFrequencies[9]), // A
+    calculateStepSize(noteFrequencies[10]), // A#
+    calculateStepSize(noteFrequencies[11]) // B
+};
+
+
 
 void loop()
 {
@@ -123,22 +181,43 @@ void loop()
   u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
   u8g2.drawStr(2, 10, "Helllo World!"); // write something to the internal memory
   u8g2.setCursor(2, 20);
-  // u8g2.print(count++);
-  // Place a for loop around your call to readCols().
-  // This will be the key scanning loop and it should loop over the row numbers 0 to 2.
-  // For each row, it should set the row select address then read the columns and
-  // copy the results into the inputs bitset
-  // loop over 1,2,4
-  for (int i = 0; i < 8; i++)
+
+  // key scanning loop and it loops over the row numbers 0 to 2.
+  for (int i = 0; i < 3; i++)
   {
     setRow(i);
+    delayMicroseconds(3);
     std::bitset<4> temp = readCols();
     inputs[i * 4] = temp[0];
     inputs[i * 4 + 1] = temp[1];
     inputs[i * 4 + 2] = temp[2];
     inputs[i * 4 + 3] = temp[3];
   }
-  std::bitset<4> inputs = readCols();
+
+  //Add code to your main loop that will check the state of each key in inputs and 
+  //look up the corresponding step size in the stepSizes array if the key is pressed
+  // If the key is not pressed, the step size should be set to 0
+  if (inputs == 0xFFF) {
+    currentStepSize = 0;
+  }
+  bool notePrinted = false; 
+  const char* notes[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+  for (int i = 0; i < 12; i++)
+  {
+    if (!inputs[i])
+    {
+      currentStepSize = stepSizes[i];
+      if (!notePrinted) {
+            // Print the note if it hasn't been printed yet
+            u8g2.setCursor(2, 30);
+            u8g2.print("Note: ");
+            u8g2.print(notes[i]); // Assuming the notes are labeled as 'C' to 'B'
+            notePrinted = true; // Set the flag to indicate the note has been printed
+      }
+      break; // Exit the loop once a key is pressed
+    }
+  }
+
   u8g2.setCursor(2, 20); // print the result on the OLED display at coordinates (2,20)
   u8g2.print(inputs.to_ulong(), HEX);
   u8g2.sendBuffer(); // transfer internal memory to the display
