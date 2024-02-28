e68406057f08efc6a5c58e1e3a8b725da70508cd
jessc21 <yc3721@ic.ac.uk>
2024-02-28 12:56:35 +0000

    complete lab1

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <U8g2lib.h>
 #include <bitset>
 #include <cmath>
-//#include <STM32HardwareTimer.h>
+#include <STM32FreeRTOS.h>
 
 // Constants
 const uint32_t interval = 100; // Display update interval
@@ -40,6 +40,11 @@ volatile uint32_t currentStepSize;
 // Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
+struct
+{
+  std::bitset<32> inputs;
+} sysState;
+
 std::bitset<4> readCols()
 {
   std::bitset<4> result;
@@ -73,15 +78,131 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value)
   digitalWrite(REN_PIN, LOW);
 }
 
-// Write a new function that will update the phase accumulator and 
+// Write a new function that will update the phase accumulator and
 // set the analogue output voltage at each sample interval
-// The function will be triggered by an interrupt 22,000 times per second. 
+// The function will be triggered by an interrupt 22,000 times per second.
 // It will add currentStepSize to the phase accumulator to generate the output waveform.
-void sampleISR() {
+void sampleISR()
+{
   static uint32_t phaseAcc = 0;
   phaseAcc += currentStepSize;
   int32_t Vout = (phaseAcc >> 24) - 128;
   analogWrite(OUTR_PIN, Vout + 128);
+}
+
+// Function to calculate step size based on frequency
+uint32_t calculateStepSize(float frequency)
+{
+  return static_cast<uint32_t>((std::pow(2, 32) * frequency) / 22000);
+}
+
+// Define frequencies for each note (in Hz)
+const float noteFrequencies[] = {
+    261.63, // C
+    277.18, // C#
+    293.66, // D
+    311.13, // D#
+    329.63, // E
+    349.23, // F
+    369.99, // F#
+    392.00, // G
+    415.30, // G#
+    440.00, // A
+    466.16, // A#
+    493.88  // B
+};
+
+// Calculate and store step sizes for each note
+const uint32_t stepSizes[] = {
+    calculateStepSize(noteFrequencies[0]),  // C
+    calculateStepSize(noteFrequencies[1]),  // C#
+    calculateStepSize(noteFrequencies[2]),  // D
+    calculateStepSize(noteFrequencies[3]),  // D#
+    calculateStepSize(noteFrequencies[4]),  // E
+    calculateStepSize(noteFrequencies[5]),  // F
+    calculateStepSize(noteFrequencies[6]),  // F#
+    calculateStepSize(noteFrequencies[7]),  // G
+    calculateStepSize(noteFrequencies[8]),  // G#
+    calculateStepSize(noteFrequencies[9]),  // A
+    calculateStepSize(noteFrequencies[10]), // A#
+    calculateStepSize(noteFrequencies[11])  // B
+};
+
+void scanKeysTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 50 / portTICK_PERIOD_MS; // initiation interval: 50ms
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  // define a local currentStepSize variable to store the current step size
+  uint32_t currentStepSizeLocal;
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    currentStepSizeLocal = 0;
+    // key scanning loop and it loops over the row numbers 0 to 2.
+    for (int i = 0; i < 3; i++)
+    {
+      setRow(i);
+      delayMicroseconds(3);
+      std::bitset<4> temp = readCols();
+      sysState.inputs[i * 4] = temp[0];
+      sysState.inputs[i * 4 + 1] = temp[1];
+      sysState.inputs[i * 4 + 2] = temp[2];
+      sysState.inputs[i * 4 + 3] = temp[3];
+    }
+
+    // Add code to your main loop that will check the state of each key in inputs and
+    // look up the corresponding step size in the stepSizes array if the key is pressed
+    //  If the key is not pressed, the step size should be set to 0
+    if (sysState.inputs == 0xFFF)
+    {
+      currentStepSize = 0;
+    }
+    for (int i = 0; i < 12; i++)
+    {
+      if (!sysState.inputs[i])
+      {
+        currentStepSizeLocal = stepSizes[i];
+        break; // Exit the loop once a key is pressed
+      }
+    }
+    __atomic_store_n(&currentStepSize, currentStepSizeLocal, __ATOMIC_RELAXED);
+  }
+}
+
+void displayUpdateTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 100 / portTICK_PERIOD_MS; // initiation interval: 100ms
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  bool notePrinted;
+  const char *notes[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    notePrinted = false;
+    // Update display
+    u8g2.clearBuffer();                   // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+    u8g2.drawStr(2, 10, "Helllo World!"); // write something to the internal memory
+    u8g2.setCursor(2, 20);
+    u8g2.print(sysState.inputs.to_ulong(), HEX);
+    for (int i = 0; i < 12; i++)
+    {
+      if (!sysState.inputs[i])
+      {
+        if (!notePrinted)
+        {
+          // Print the note if it hasn't been printed yet
+          u8g2.setCursor(2, 30);
+          u8g2.print(notes[i]);
+          // Serial.println(notes[i]);
+          notePrinted = true; // Set the flag to indicate the note has been printed
+        }
+        break; // Exit the loop once a key is pressed
+      }
+    }
+    u8g2.sendBuffer(); // transfer internal memory to the display
+    // digitalToggle(LED_BUILTIN);
+  }
 }
 
 void setup()
@@ -122,106 +243,28 @@ void setup()
   // Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  // initialise and run the thread
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+      displayUpdateTask,
+      "displayUpdate",
+      256,
+      NULL,
+      1,
+      &displayUpdateHandle);
+
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+      scanKeysTask,     /* Function that implements the task */
+      "scanKeys",       /* Text name for the task */
+      64,               /* Stack size in words, not bytes */
+      NULL,             /* Parameter passed into the task */
+      2,                /* Task priority */
+      &scanKeysHandle); /* Pointer to store the task handle */
+
+  // start the RTOS scheduler
+  vTaskStartScheduler();
 }
 
-// Function to calculate step size based on frequency
-uint32_t calculateStepSize(float frequency)
-{
-    return static_cast<uint32_t>((std::pow(2, 32) * frequency) / 22000);
-}
-
-// Define frequencies for each note (in Hz)
-const float noteFrequencies[] = {
-    261.63, // C
-    277.18, // C#
-    293.66, // D
-    311.13, // D#
-    329.63, // E
-    349.23, // F
-    369.99, // F#
-    392.00, // G
-    415.30, // G#
-    440.00, // A
-    466.16, // A#
-    493.88  // B
-};
-
-// Calculate and store step sizes for each note
-const uint32_t stepSizes[] = {
-    calculateStepSize(noteFrequencies[0]), // C
-    calculateStepSize(noteFrequencies[1]), // C#
-    calculateStepSize(noteFrequencies[2]), // D
-    calculateStepSize(noteFrequencies[3]), // D#
-    calculateStepSize(noteFrequencies[4]), // E
-    calculateStepSize(noteFrequencies[5]), // F
-    calculateStepSize(noteFrequencies[6]), // F#
-    calculateStepSize(noteFrequencies[7]), // G
-    calculateStepSize(noteFrequencies[8]), // G#
-    calculateStepSize(noteFrequencies[9]), // A
-    calculateStepSize(noteFrequencies[10]), // A#
-    calculateStepSize(noteFrequencies[11]) // B
-};
-
-
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-  std::bitset<32> inputs;
-
-  while (millis() < next)
-    ; // Wait for next interval
-
-  next += interval;
-
-  // Update display
-  u8g2.clearBuffer();                   // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
-  u8g2.drawStr(2, 10, "Helllo World!"); // write something to the internal memory
-  u8g2.setCursor(2, 20);
-
-  // key scanning loop and it loops over the row numbers 0 to 2.
-  for (int i = 0; i < 3; i++)
-  {
-    setRow(i);
-    delayMicroseconds(3);
-    std::bitset<4> temp = readCols();
-    inputs[i * 4] = temp[0];
-    inputs[i * 4 + 1] = temp[1];
-    inputs[i * 4 + 2] = temp[2];
-    inputs[i * 4 + 3] = temp[3];
-  }
-
-  //Add code to your main loop that will check the state of each key in inputs and 
-  //look up the corresponding step size in the stepSizes array if the key is pressed
-  // If the key is not pressed, the step size should be set to 0
-  if (inputs == 0xFFF) {
-    currentStepSize = 0;
-  }
-  bool notePrinted = false; 
-  const char* notes[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
-  for (int i = 0; i < 12; i++)
-  {
-    if (!inputs[i])
-    {
-      currentStepSize = stepSizes[i];
-      if (!notePrinted) {
-            // Print the note if it hasn't been printed yet
-            u8g2.setCursor(2, 30);
-            u8g2.print("Note: ");
-            u8g2.print(notes[i]); // Assuming the notes are labeled as 'C' to 'B'
-            notePrinted = true; // Set the flag to indicate the note has been printed
-      }
-      break; // Exit the loop once a key is pressed
-    }
-  }
-
-  u8g2.setCursor(2, 20); // print the result on the OLED display at coordinates (2,20)
-  u8g2.print(inputs.to_ulong(), HEX);
-  u8g2.sendBuffer(); // transfer internal memory to the display
-
-  // Toggle LED
-  digitalToggle(LED_BUILTIN);
-}
+void loop() {}
